850ad7c0a1f6b1da22a0130aed5be1e8f1f2e9f5
KL012 <kevl927@gmail.com>
2016-11-22 16:49:51 +0000

    add web address in whitelist

--- server/config/cors.js
+++ server/config/cors.js
@@ -2,10 +2,12 @@
 // exactly as it would appear in the Origin header
 // e.g. 'https://www.myurl.com'
 
-const whitelist = ['https://sup-messaging-frontend-v2-kl012.c9users.io'];
+const whitelist = ['https://sup-messaging-frontend-v2-kl012.c9users.io','https://matcodesalot.github.io'];
 
 module.exports = (req, res, next) => {
     let allowCORS = false;
+    
+    console.log(req.headers.origin)
     
     if (process.env.NODE_ENV !== 'production') allowCORS = true;
     if (process.env.NODE_ENV === 'production' && whitelist.indexOf(req.headers.origin) !== -1) allowCORS = true;
