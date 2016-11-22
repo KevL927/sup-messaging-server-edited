aae4e52efc37f4710e90e42dd2911cac5047ef5b
KL012 <kevl927@gmail.com>
2016-11-22 01:24:23 +0000

    fix CORS

--- server/index.js
+++ server/index.js
@@ -11,8 +11,8 @@ const messagesRouter = require('./routes/messages');
 const usersRouter = require('./routes/users');
 
 const app = express();
-app.options('/api/v1/*', function(req, res) { res.sendStatus(200)} );
 app.use('/api/v1/*', setCORS);
+app.options('/api/v1/*', function(req, res) { res.sendStatus(200)} );
 app.post('*', jsonParser);
 app.put('*', jsonParser);
 app.use('/api/v1/messages', messagesRouter);
